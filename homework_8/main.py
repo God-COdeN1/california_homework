5a7f30fa98fc48acdd4d260bcc711775b805b492
God-COdeN1 <aniartom86@gmail.com>
2023-10-12 19:30:49 +0300

    About me

--- homework_8/main.py
+++ homework_8/main.py
@@ -10,19 +10,35 @@ my_int = 77
 print(type(my_int), "Type ob")
 print(id(my_int))
 
-result_addition = my_string + str(my_int)
-my_str_to_int = int(my_string)
-result_subtraction = my_str_to_int - my_int
+# result_addition = my_string + str(my_int)
+# my_str_to_int = int(my_string)
+# result_subtraction = my_str_to_int - my_int
 
-# Перетворення числового типу даних в строку і навпаки
-num_1 = 36
-num_2 = 168
-num_3 = 25.65
+# num_1 = 36
+# num_2 = 168
+# num_3 = 25.65
 
-str_1 = str(num_1)
-str_2 = str(num_2)
-str_3 = str(num_3)
+# str_1 = str(num_1)
+# str_2 = str(num_2)
+# str_3 = str(num_3)
 
-num_from_str_1 = int(str_1)
-num_from_str_2 = int(str_2)
-num_from_str_3 = float(str_3)
+# num_from_str_1 = int(str_1)
+# num_from_str_2 = int(str_2)
+# num_from_str_3 = float(str_3)
+
+name = "artem"
+last_name = "anistratenko"
+year = "16"
+country = "ukraine"
+pet = "slime"
+book = "the lord of the rings"
+play = "chess"
+hobby = "video editing"
+eat = "dumplings"
+
+print(
+    f"""My name is {name.capitalize()} and my last name {last_name.capitalize()}\n. About me:\t\nAge: {year}
+      I live in {country.capitalize()}
+      My favorite series books is {book.title()}
+      My favorite game is {play.capitalize()}"""
+)
